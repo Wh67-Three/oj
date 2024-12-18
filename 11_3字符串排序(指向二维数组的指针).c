bdf063462a240987bb2e95cf7e69521d1a2e62a3
wh67 <2350279436@qq.com>
2024-12-18 14:40:02 +0800

    Create 11_3字符串排序(指向二维数组的指针).c
    描述
    
    采用指向二维数组的指针，对字符串排序。
    
    在主函数中输入 10 个字符串。调用 sort 函数对它们排序，然后在主函数中输出这10 个已经排好序的字符串。
    
    要求：
    
    1）采用指针数组实现；
    
    3) 可以调用 strcmp 函数。
    
    
    输入
    
    输入 10 个字符串（长度不超过100）。
    
    
    输出
    
    输出为排序之后的结果。
    
    
    输入样例 1 
    
    bb dd cc ee ff gg hh cc dd zz
    输出样例 1
    
    bb cc cc dd dd ee ff gg hh zz

---
+++ 11_3字符串排序(指向二维数组的指针).c
@@ -0,0 +1,30 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<string.h>
+void sort(char(*arr)[105]) {
+	char m[105];
+	const char (*brr)[105];
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10 - i - 1; j++) {
+			brr = arr;
+			if (strcmp(*(brr + j),*(brr + j + 1)) > 0) {
+				strcpy(m, arr[j + 1]);
+				strcpy(arr[j + 1], arr[j]);
+				strcpy(arr[j], m);
+			}
+		}
+	}
+}
+int main() {
+	int n = 10;
+	char arr[10][105];
+	for (int i = 0; i < 10; i++) {
+		scanf("%s", arr + i);
+	}
+	char (* m)[105] = arr;
+	sort(m);
+	for (int i = 0; i < 10; i++) {
+		printf("%s ", *(m+i));
+	}
+	return 0;
+}
